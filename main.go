ecf96f26842ea31762db48e2cb43ed4b18e7dfb2
Jakub Petrovic <petrovic.jakub@hotmail.com>
2023-07-21 21:50:21 +0200

    add command map and help function

--- main.go
+++ main.go
@@ -2,19 +2,47 @@ package main
 
 import "fmt"
 
+type cliCommands struct {
+	name        string
+	description string
+	//callback    func() error
+}
+
+func commandHelp(commandMap map[string]cliCommands) {
+	fmt.Printf("\n\nWelcome to Pokedex!\n\n")
+	for _, n := range commandMap {
+		fmt.Printf("%s: %s\n", n.name, n.description)
+	}
+	fmt.Printf("\n")
+}
+
 func main() {
+	m := map[string]cliCommands{
+		"help": {
+			name:        "help",
+			description: "Display a help message",
+			//callback:    commandHelp(),
+		},
+		"exit": {
+			name:        "exit",
+			description: "Exits the Pokedex",
+			//callback:    commandExit(),
+		},
+	}
+
 	for {
 		var input string = ""
-		fmt.Println("pokedex > ")
+		fmt.Printf("pokedex > ")
 		fmt.Scanln(&input)
 
-		if input == "help" {
-			fmt.Println("NO HALP!! pres any key to continue...")
-			fmt.Scanln()
-		}
-
-		if input == "exit" {
+		switch input {
+		case "exit":
+			fmt.Println("see you next time")
 			return
+		case "help":
+			commandHelp(m)
+		default:
+			fmt.Println("incorrect output")
 		}
 
 	}
