b9c97be6c4eab7ace1a390b4eb1747bd5788d838
Jakub Petrovič <petrovic.jakub@hotmail.com>
2023-07-22 23:48:17 +0200

    prepare cache package

--- internal/pokecache/pokecache.go
+++ internal/pokecache/pokecache.go
@@ -1,1 +1,33 @@
 package pokecache
+
+import (
+	"sync"
+	"time"
+)
+
+type cacheEntry struct {
+	Data      string
+	Timestamp time.Time
+}
+
+type Cache struct {
+	dataMap map[string]cacheEntry
+	mutex   sync.Mutex
+}
+
+func NewCache(t time.Duration) {
+
+}
+
+func Add(key string, value []byte) {
+
+}
+
+func Get(key string) ([]byte, bool) {
+
+	return []byte, false
+}
+
+func reapLoop() {
+
+}
